616b8490b256b9a1bfaf1ba21dbceab752996d5b
Inee Ader <xoliio@gmail.com>
2020-08-31 21:19:22 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,12 @@
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario
   greeting = 'It's-a me, Mario!'
   puts greeting
 end
+puts mario
 
 def toadstool
-  
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+puts toadstool
